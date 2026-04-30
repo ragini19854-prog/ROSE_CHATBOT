cfc3f4d13fadb76a7202e48ff65381b1cb15a82c
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:28:33 +0530

    Update bot.js

--- hinata-bot/src/config/bot.js
+++ hinata-bot/src/config/bot.js
@@ -2,23 +2,28 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const config = require('./config/index');
 const logger = require('./utils/logger');
+
+// Load DB
+require('./models/index');
+
+// Import handlers
 const startCommand = require('./handlers/commands/start');
 const handleCallbacks = require('./handlers/callbacks');
 
 const bot = new Telegraf(config.botToken);
 
-// Start Command
+// Commands
 bot.start(startCommand);
+bot.command('ping', (ctx) => ctx.reply('🏓 Pong!'));
 
 // Callbacks
 bot.on('callback_query', handleCallbacks);
 
-// Basic ping
-bot.command('ping', (ctx) => ctx.reply('🏓 Pong!'));
+// TODO: Add AI Moderation + Group Commands later
 
 bot.launch()
-  .then(() => logger.info('🚀 Hinata Bot started successfully'))
-  .catch(err => logger.error('Bot launch failed', err));
+  .then(() => logger.info('🚀 Hinata Bot is running...'))
+  .catch(err => logger.error('Launch Error', err));
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
