a212e954035ca56388714c1f229aa4b26fcc2ee4
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:10:46 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  userId: { type: Number, required: true, unique: true },
+  username: String,
+  firstName: String,
+  coins: { type: Number, default: 0 },
+  warnings: { type: Number, default: 0 },
+  isBanned: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
