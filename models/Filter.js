ef8f42e414530023efa06fa8a2086ffc5d30e3e9
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 23:02:45 +0530

    Update Filter.js

--- models/Filter.js
+++ models/Filter.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const filterSchema = new mongoose.Schema({
+  chatId: { type: Number, required: true },
+  word: { type: String, required: true },
+  action: { type: String, enum: ['delete', 'warn', 'mute'], default: 'delete' },
+  createdBy: Number
+}, { timestamps: true });
+
+module.exports = mongoose.model('Filter', filterSchema);
