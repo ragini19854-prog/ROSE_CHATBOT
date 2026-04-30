8d440cb5ed1a564fb10767bb93b4ceefb175f016
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 21:57:56 +0530

    Update rateLimit.js

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,1 +1,15 @@
+const rateLimit = new Map();
 
+const cooldown = (ms) => async (ctx, next) => {
+    const key = `${ctx.from.id}:${ctx.chat?.id}`;
+    const now = Date.now();
+
+    if (rateLimit.has(key) && now - rateLimit.get(key) < ms) {
+        return; // silent ignore
+    }
+
+    rateLimit.set(key, now);
+    return next();
+};
+
+module.exports = cooldown;
