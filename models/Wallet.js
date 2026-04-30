b647f318c142da8f7865de19d5ae564e8fdd4379
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:16:00 +0530

    Update Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const walletSchema = new mongoose.Schema({
+  userId: { type: Number, required: true, unique: true },
+  coins: { type: Number, default: 0 },
+  lastDaily: Date,
+  lastWeekly: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model('Wallet', walletSchema);
