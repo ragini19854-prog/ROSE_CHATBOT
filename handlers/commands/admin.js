2e23b6736807b9d3dda9509088f686a27390b441
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:44:03 +0530

    Update admin.js

--- handlers/commands/admin.js
+++ handlers/commands/admin.js
@@ -2,14 +2,19 @@ const config = require('../config/index');
 
 const isAdmin = async (ctx, next) => {
     if (!ctx.chat || ctx.chat.type === 'private') return next();
-    
-    const member = await ctx.getChatMember(ctx.from.id);
-    const isAdmin = ['administrator', 'creator'].includes(member.status);
-    const isOwner = config.sudoUsers.includes(ctx.from.id) || ctx.from.id === config.ownerId;
 
-    ctx.isAdmin = isAdmin || isOwner;
-    ctx.isOwner = isOwner;
-    
+    try {
+        const member = await ctx.getChatMember(ctx.from.id);
+        const isAdmin = ['administrator', 'creator'].includes(member.status);
+        const isOwner = config.sudoUsers.includes(ctx.from.id) || ctx.from.id === config.ownerId;
+
+        ctx.isAdmin = isAdmin || isOwner;
+        ctx.isOwner = isOwner;
+    } catch (e) {
+        ctx.isAdmin = false;
+        ctx.isOwner = false;
+    }
+
     return next();
 };
 
