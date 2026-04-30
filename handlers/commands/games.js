62778f999f4f74732f9717e02044cb8296387032
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:36:27 +0530

    Update games.js

--- handlers/commands/games.js
+++ handlers/commands/games.js
@@ -1,1 +1,9 @@
+async function wordGuess(ctx) {
+    await ctx.reply('🎮 Word Guess game coming soon...');
+}
 
+async function gamew(ctx) {
+    await ctx.reply('🎮 Word Streak game coming soon...');
+}
+
+module.exports = { wordGuess, gamew };
