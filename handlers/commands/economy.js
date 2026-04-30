b1bada9c5a3309eeb7b0fab65badc9fb14ad9912
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:34:52 +0530

    Update economy.js

--- handlers/commands/economy.js
+++ handlers/commands/economy.js
@@ -1,1 +1,8 @@
+const { getBalance } = require('../../services/economyService');
 
+async function balanceCommand(ctx) {
+    const balance = await getBalance(ctx.from.id);
+    await ctx.reply(`💰 Your balance: **${balance} coins**`, { parse_mode: 'Markdown' });
+}
+
+module.exports = { balanceCommand };
