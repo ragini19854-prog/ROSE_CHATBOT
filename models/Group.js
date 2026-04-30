4335259886616775e88f136d5d2d1c4e0c81a770
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:04:49 +0530

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const groupSchema = new mongoose.Schema({
+  chatId: { type: Number, required: true, unique: true },
+  title: String,
+  settings: {
+    welcomeEnabled: { type: Boolean, default: true },
+    goodbyeEnabled: { type: Boolean, default: true },
+    antiflood: { type: Boolean, default: true },
+    nsfwProtection: { type: Boolean, default: true },
+    strictMode: { type: Boolean, default: false }
+  },
+  rules: String,
+  welcomeMessage: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Group', groupSchema);
