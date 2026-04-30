91eae87a170a40adf7fcdf7380e174c637aea35a
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 23:09:30 +0530

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const noteSchema = new mongoose.Schema({
+  chatId: { type: Number, required: true },
+  name: { type: String, required: true },
+  content: { type: String, required: true },
+  createdBy: Number
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', noteSchema);
