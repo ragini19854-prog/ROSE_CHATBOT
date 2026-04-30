b242624f2d4d4e97f1f68998588919c4053ded67
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 23:17:43 +0530

    Update loggingService.js

--- services/loggingService.js
+++ services/loggingService.js
@@ -1,1 +1,9 @@
+const logger = require('../utils/logger');
 
+const loggingService = {
+  logModeration: (chatId, userId, action, reason) => {
+    logger.info(`[MOD] Chat:${chatId} User:${userId} Action:${action} Reason:${reason}`);
+  }
+};
+
+module.exports = loggingService;
