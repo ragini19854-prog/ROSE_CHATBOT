96244eccec6275d816802e2ff4ba5d43070a8c82
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 10:15:53 +0530

    Update bot.js

--- hinata-bot/src/config/bot.js
+++ hinata-bot/src/config/bot.js
@@ -1,1 +1,20 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const config = require('./config/index');
+const logger = require('./utils/logger');
+
+// Bot instance
+const bot = new Telegraf(config.botToken);
+
+bot.start((ctx) => ctx.reply('🌸 Hi! I am Hinata. Ready to protect your group!'));
+
+bot.launch()
+  .then(() => logger.info('🚀 Hinata Bot started successfully'))
+  .catch(err => logger.error('Bot launch failed', err));
+
+// Graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+module.exports = bot;
 
