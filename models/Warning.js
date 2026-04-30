1bed13059e1a18b313a34d6500dcd040624324ca
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 23:12:47 +0530

    Update Warning.js

--- models/Warning.js
+++ models/Warning.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const warningSchema = new mongoose.Schema({
+  userId: { type: Number, required: true },
+  chatId: { type: Number, required: true },
+  count: { type: Number, default: 0 },
+  reasons: [{ reason: String, date: Date }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Warning', warningSchema);
