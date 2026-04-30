42abfeb71897a686377fc0450eeda91d88a9a73d
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:16:22 +0530

    Update ChatMemory.js

--- models/ChatMemory.js
+++ models/ChatMemory.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const chatMemorySchema = new mongoose.Schema({
+  userId: Number,
+  chatId: Number,
+  messages: [{ role: String, content: String, timestamp: Date }],
+  lastUpdated: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+module.exports = mongoose.model('ChatMemory', chatMemorySchema);
