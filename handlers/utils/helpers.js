df6bd4e60c9fe27d141c80c0837bbc0659594ec3
ragini19854-prog <ragini.19854@gmail.com>
2026-04-30 22:58:04 +0530

    Update helpers.js

--- handlers/utils/helpers.js
+++ handlers/utils/helpers.js
@@ -1,1 +1,11 @@
+const helpers = {
+  escapeHtml: (text) => {
+    return text ? text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;') : '';
+  },
+  
+  getUserMention: (user) => {
+    return user.username ? `@${user.username}` : user.first_name;
+  }
+};
 
+module.exports = helpers;
